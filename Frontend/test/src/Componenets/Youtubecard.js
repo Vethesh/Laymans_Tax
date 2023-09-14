6f933d9c9a8567532db30a8f32b7bf98aa8be84f
Vethesh <vetheshkumar7@gmail.com>
2023-09-14 10:48:39 +0530

    navbar updated

--- Frontend/test/src/Componenets/Youtubecard.js
+++ Frontend/test/src/Componenets/Youtubecard.js
@@ -2,29 +2,22 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 // import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
 
 import { Button, CardActionArea, CardActions } from "@mui/material";
 
-export default function Youtubecard({cardDetail}) {
+export default function Youtubecard({ cardDetail }) {
   return (
-    <Card sx={{ maxWidth: 600,boxShadow:"1px 1px 5px black", height:450,}}>
-      <CardActionArea>
-        {/* <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        /> */}
-        <CardContent>
-         
-        
-         
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-       
-      </CardActions>
-    </Card>
+    <div className="video">
+      <iframe
+        width="600px"
+        height="450px"
+        src={cardDetail.title}
+        key={cardDetail.id}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+    </div>
   );
 }
--- Frontend/test/src/Pages/Home.js
+++ Frontend/test/src/Pages/Home.js
@@ -3,35 +3,73 @@ import Layout from "../Componenets/Layout";
 import Img from "../Images/front.png";
 import Youtubecard from "../Componenets/Youtubecard";
 
-// import Cards from "../Componenets/Cards";
-
 const Home = () => {
+  const data = [
+    {
+      id: 1,
+      title: "https://www.youtube.com/embed/XTKgvVcqnNQ?si=0LbZBp5yvi17me94",
+    },
+    {
+      id: 2,
+      title: "https://www.youtube.com/embed/OJiXG4Pnwhc?si=kIuNhr7UolkV85Ky",
+    },
+    {
+      id: 3,
+      title: "https://www.youtube.com/embed/3g61afhXcQg?si=gQ6lIJHh9zM-2XQu",
+    },
+    {
+      id: 4,
+      title: "https://www.youtube.com/embed/JTpDt9HpO5E?si=Jn2-9WDKMQ3GtcXu",
+    },
+    {
+      id: 5,
+      title: "https://www.youtube.com/embed/ElBuWNVXxSQ?si=PIGlUsH-ufJEVxXu" ,
+    },
+    {
+      id: 6,
+      title: "https://www.youtube.com/embed/mJc5stZ1NvM?si=rTSUy41GL5y9F8-E",
+    },
+    {
+      id: 7,
+      title: "https://www.youtube.com/embed/qQgPrWaC9FU?si=_8izNUOMZEVcVdfe",
+    },
+    {
+      id: 8,
+      title: "https://www.youtube.com/embed/iwpHQH4bkMA?si=My1K-zL5EZKxcNBy",
+    },
+  ];
+
   return (
     <Layout>
       <div className="main">
-        {/* <div className="text">
-            <p>Welcome to Tax Consultant</p>
-          </div> */}
-        <div className="bg">
-          <img
-            src={Img}
-            alt="bg"
-            style={
-              {
-                //   display: "flex",
-                //   justifyContent: "center",
-                // margin: "5% 10%",
-                //    width: "300px",
-              }
-            }
-          />
-
-          <div className="youtube">
-            <Youtubecard />
-          </div>
+        <div className="text-slider">
+          <marquee>
+            We have revolutionized the tax filing process in India, not only
+            ensuring the swift completion of your tax return but also securing
+            the highest possible refunds for you in record time.
+          </marquee>
         </div>
 
-        {/* <Cards/> */}
+        <div
+          className="bg"
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <div>
+            <img src={Img} alt="bg" />
+          </div>
+
+          <div className="youtube">
+            {data.map((ele) => (
+              <Youtubecard key={ele.id} cardDetail={ele} />
+            ))}
+          </div>
+        </div>
       </div>
     </Layout>
   );
